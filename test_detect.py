f25f009fef40a8347aa1c78e82c9800a4e40d58c
Bobby Liu <lp9052@gmail.com>
2026-05-10 00:01:47 -0400

    refactor: remove obsolete test and simulation scripts from the project root

--- test_detect.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.abspath('.'))
-from app.auditor.signal_detector import detect_signals, detect_signals_semantic
-print(detect_signals_semantic("I want to by $20000 APPL put options"))
-print(detect_signals_semantic("i want to buy $5000 APPL put options"))
